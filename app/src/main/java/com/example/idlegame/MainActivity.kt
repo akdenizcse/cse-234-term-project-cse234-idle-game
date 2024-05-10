f193f059bfcc6f0b8d70e65a9e22f0f02b6b2fcc
Cherub26 <110939979+Cherub26@users.noreply.github.com>
2024-05-10 21:20:22 +0300

    DownBar added and implemented onto WeaponsScreen

--- app/src/main/java/com/example/idlegame/MainActivity.kt
+++ app/src/main/java/com/example/idlegame/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -28,6 +29,8 @@ import androidx.compose.ui.unit.dp
 import com.example.idlegame.ui.theme.IdleGameTheme
 import com.example.idlegame.weapon.Weapon
 import com.example.idlegame.data.WeaponData
+import com.example.idlegame.downbar.Design
+import com.example.idlegame.downbar.DownBar
 import com.example.idlegame.upbar.UpBar
 
 class MainActivity : ComponentActivity() {
@@ -40,7 +43,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = Color(0xFF373737)
                 ) {
-                    Greeting()
+                    WeaponsScreen()
                 }
             }
         }
@@ -48,7 +51,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(modifier: Modifier = Modifier) {
+fun WeaponsScreen() {
     val weapons = listOf(
         WeaponData("Grim Reapersssss", "12 LVL", "/s", "802.12M", R.drawable.weapon_photo),
         WeaponData("title2", "11 LVL", "/s", "123.31k", R.drawable.weapon_photo),
@@ -63,32 +66,45 @@ fun Greeting(modifier: Modifier = Modifier) {
         WeaponData("title11", "2 LVL", "/s", "123.31k", R.drawable.weapon_photo),
         WeaponData("title12", "1 LVL", "/s", "123.31k", R.drawable.weapon_photo)
     )
-    Column(horizontalAlignment = Alignment.CenterHorizontally) {
-        UpBar(output = "120k/s",
-            onGear = {},
-            money = "911m",
-            gems = "30",
-            modifier = Modifier.fillMaxWidth())
-        LazyColumn(modifier = Modifier.fillMaxWidth().align(Alignment.CenterHorizontally)) {
-            items(weapons) { weapon ->
-                Weapon(
-                    title = weapon.title,
-                    level = weapon.level,
-                    income = weapon.income,
-                    price = weapon.price,
-                    weaponPicture = painterResource(weapon.weaponPicture),
-                    modifier = Modifier.padding(5.dp, 8.dp)
-                )
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column {
+            UpBar(
+                output = "120k/s",
+                onGear = {},
+                money = "911m",
+                gems = "30",
+                modifier = Modifier.fillMaxWidth()
+            )
+            LazyColumn(modifier = Modifier.weight(1f).fillMaxWidth()) {
+                items(weapons) { weapon ->
+                    Weapon(
+                        title = weapon.title,
+                        level = weapon.level,
+                        income = weapon.income,
+                        price = weapon.price,
+                        weaponPicture = painterResource(weapon.weaponPicture),
+                        modifier = Modifier.padding(5.dp, 8.dp)
+                    )
+                }
             }
+            Spacer(modifier = Modifier.height(60.dp)) // Might need adjusting not sure if 60.dp is correct
         }
+        DownBar(
+            onWeaponsTab = {},
+            onStoreTab = {},
+            onUpgradesTab = {},
+            design = Design.WeaponsTab,
+            modifier = Modifier.align(Alignment.BottomCenter)
+        )
     }
 
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun WeaponsScreenPreview() {
     IdleGameTheme {
-        Greeting()
+        WeaponsScreen()
     }
 }
