284e7498eea1c0119e7bc1afe4e2d3bcf3c58954
Batuhan <bthnylmz028@gmail.com>
2024-05-17 22:53:40 +0300

    Enemy Animation Added

--- app/src/main/java/com/example/idlegame/MainActivity.kt
+++ app/src/main/java/com/example/idlegame/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.example.idlegame
 
-import android.annotation.SuppressLint
+import Enemies
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,13 +11,10 @@ import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import com.example.idlegame.ui.screens.Enemies
-import com.example.idlegame.ui.screens.slimeEnemy
-import com.example.idlegame.ui.screens.slimeEnemy2
 import com.example.idlegame.ui.theme.IdleGameTheme
+import slimeEnemy
 
 class MainActivity : ComponentActivity() {
-    @SuppressLint("UnrememberedMutableState")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -28,7 +25,7 @@ class MainActivity : ComponentActivity() {
                     color = Color(0xFF373737)
                 ) {
                     Box(modifier =Modifier.padding(16.dp)) {
-                        val enemyList = mutableListOf(slimeEnemy, slimeEnemy2)
+                        val enemyList = mutableListOf(slimeEnemy)
                         Enemies(enemies = enemyList)
                     }
                 }
--- app/src/main/java/com/example/idlegame/ui/screens/Enemy.kt
+++ app/src/main/java/com/example/idlegame/ui/screens/Enemy.kt
@@ -1,21 +1,10 @@
-package com.example.idlegame.ui.screens
-
+import androidx.compose.animation.core.*
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -25,14 +14,12 @@ import com.example.idlegame.R
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
-
-
 enum class LifeState {
     ALIVE,
     DEAD,
     RESPAWNING
-
 }
+
 data class Enemy(
     val name: String,
     var health: Int,
@@ -49,7 +36,7 @@ val slimeEnemy = Enemy(
         val imageResource by enemy.imageResource
         Image(painterResource(id = imageResource), contentDescription = "Slime Icon", Modifier.size(64.dp))
     },
-    imageResource = mutableStateOf(R.drawable.slime),
+    imageResource = mutableStateOf(R.drawable.slimeidle1), // Initial drawable
     lifeState = mutableStateOf(LifeState.ALIVE)
 )
 
@@ -57,6 +44,63 @@ val slimeEnemy = Enemy(
 fun Enemy(enemy: Enemy) {
     val state = remember { mutableStateOf(enemy.health) }
     val scope = rememberCoroutineScope()
+
+    // Array of drawable resources
+    val drawableResources = listOf(
+        R.drawable.slimeidle1,
+        R.drawable.slimeidle2,
+        R.drawable.slimeidle3,
+        R.drawable.slimeidle4,
+        R.drawable.slimeidle5,
+        R.drawable.slimeidle6,
+        R.drawable.slimeidle7,
+    )
+    val deathDrawableResources = listOf(
+        R.drawable.slimedeath1,
+        R.drawable.slimedeath2,
+        R.drawable.slimedeath3,
+        R.drawable.slimedeath4,
+        R.drawable.slimedeath5,
+        R.drawable.slimedeath6,
+        R.drawable.slimedeath7,
+        R.drawable.slimedeath8,
+        R.drawable.slimedeath9,
+        R.drawable.slimedeath10,
+        R.drawable.slimedeath11,
+        R.drawable.slimedeath12,
+        R.drawable.slimedeath13,
+    )
+
+    val infiniteTransition = rememberInfiniteTransition()
+    val frameIndex by infiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = (drawableResources.size - 1).toFloat(),
+        animationSpec = infiniteRepeatable(
+            animation = tween(1000, easing = LinearEasing),
+            repeatMode = RepeatMode.Restart
+        )
+    )
+
+    var deathFrameIndex by remember { mutableStateOf(0f) }
+    LaunchedEffect(enemy.lifeState.value) {
+        if (enemy.lifeState.value == LifeState.DEAD) {
+            deathFrameIndex = 0f
+            animate(
+                initialValue = 0f,
+                targetValue = (deathDrawableResources.size - 1).toFloat(),
+                animationSpec = tween(2000, easing = LinearEasing)
+            ) { value, _ ->
+                deathFrameIndex = value
+            }
+        }
+    }
+
+    // Update image resource based on animation frame
+    if (enemy.lifeState.value == LifeState.ALIVE) {
+        enemy.imageResource.value = drawableResources[frameIndex.toInt()]
+    } else if (enemy.lifeState.value == LifeState.DEAD) {
+        enemy.imageResource.value = deathDrawableResources[deathFrameIndex.toInt()]
+    }
 
     DisposableEffect(Unit) {
         val listener: (EnemyHitEvent) -> Unit = { event ->
@@ -78,42 +122,36 @@ fun Enemy(enemy: Enemy) {
     ) {
         Text(text = enemy.name, style = MaterialTheme.typography.bodyLarge)
         Text(text = state.value.toString(), textAlign = TextAlign.Center)
-        Box(modifier = Modifier
-            .clickable {
-                if (enemy.lifeState.value == LifeState.ALIVE) {
-                    damageEnemy(enemy, 1)
-                    scope.launch {
-                        if (enemy.lifeState.value != LifeState.DEAD) {
-                            enemy.imageResource.value = R.drawable.slime_hit
-                        }
-                        delay(200)
-                        if (enemy.lifeState.value != LifeState.DEAD) {
-                            enemy.imageResource.value = R.drawable.slime
-                        } else if (enemy.lifeState.value == LifeState.DEAD) {
-                            if (!enemy.isRespawning.value) {
-                                enemy.isRespawning.value = true
-                                delay(2000)
-                                enemy.lifeState.value = LifeState.RESPAWNING
-                                enemy.imageResource.value = R.drawable.slime_dead
-                                if (enemy.lifeState.value == LifeState.RESPAWNING) {
-                                    state.value = enemy.health + 2
-                                    enemy.health = state.value
-                                    enemy.imageResource.value = R.drawable.slime
-                                    enemy.lifeState.value = LifeState.ALIVE
-                                    enemy.isRespawning.value = false
+        Box(
+            modifier = Modifier
+                .clickable {
+                    if (enemy.lifeState.value == LifeState.ALIVE) {
+                        damageEnemy(enemy, 1)
+                        scope.launch {
+                            if (enemy.lifeState.value == LifeState.DEAD) {
+                                if (!enemy.isRespawning.value) {
+                                    enemy.isRespawning.value = true
+                                    delay(2000)
+                                    enemy.lifeState.value = LifeState.RESPAWNING
+                                    enemy.imageResource.value = R.drawable.slime_dead
+                                    if (enemy.lifeState.value == LifeState.RESPAWNING) {
+                                        state.value = enemy.health + 2
+                                        enemy.health = state.value
+                                        enemy.imageResource.value = drawableResources[frameIndex.toInt()] // back to idle animation
+                                        enemy.lifeState.value = LifeState.ALIVE
+                                        enemy.isRespawning.value = false
+                                    }
                                 }
                             }
                         }
                     }
                 }
-            }
-            .wrapContentSize()
+                .wrapContentSize()
         ) {
             enemy.icon(enemy)
         }
     }
 }
-
 
 @Composable
 fun Enemies(enemies: MutableList<Enemy>) {
@@ -123,7 +161,6 @@ fun Enemies(enemies: MutableList<Enemy>) {
         }
     }
 }
-
 
 val enemyList = mutableStateOf(mutableListOf(slimeEnemy))
 
@@ -151,6 +188,17 @@ fun damageEnemy(enemy: Enemy, damage: Int) {
     }
     val event = EnemyHitEvent(enemy, damage)
     EventBus.triggerEvent(event)
-    }
+}
 
-
+@Composable
+fun InfiniteTransition.AnimateDeathFrameIndex(
+    targetValue: Float,
+    animationSpec: InfiniteRepeatableSpec<Float>,
+    deathFrameIndex: MutableState<Float>
+) {
+    val frameIndex by animateFloatAsState(
+        targetValue = targetValue,
+        animationSpec = animationSpec
+    )
+    deathFrameIndex.value = frameIndex
+}
