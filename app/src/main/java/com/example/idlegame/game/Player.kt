8dac4fbacb04c001f3aae52b6d836d381fe183b0
Cherub26 <110939979+Cherub26@users.noreply.github.com>
2024-06-07 23:46:40 +0300

    Fixed the weapons not getting upgraded once bought

--- app/src/main/java/com/example/idlegame/game/Player.kt
+++ app/src/main/java/com/example/idlegame/game/Player.kt
@@ -1,14 +1,18 @@
 package com.example.idlegame.game
 
 
+import android.util.Log
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableDoubleStateOf
 
 class PlayerViewModel : ViewModel() {
-    val player: Player = Player(money = 10)
+    val player: Player = Player(money = 10.0)
+    val weapons: State<MutableList<WeaponGame>> get() = player.weapons
     fun earnMoney() {
         viewModelScope.launch {
             player.earnMoney()
@@ -20,20 +24,24 @@ class PlayerViewModel : ViewModel() {
         }
     }
 
+    fun setWeapons(weapons: List<WeaponGame>) {
+        player.weapons.value = weapons.toMutableList()
+    }
 }
-class Player(money: Int = 10, val weapons: MutableList<WeaponGame> = mutableListOf()) {
-    var money: MutableState<Int> = mutableStateOf(money)
+class Player(money: Double, weapons: MutableList<WeaponGame> = mutableListOf()) {
+    var money: MutableState<Double> = mutableDoubleStateOf(money)
+    var weapons: MutableState<MutableList<WeaponGame>> = mutableStateOf(weapons)
     fun earnMoney() {
-        weapons.forEach { money.value += it.damage().toInt() }
+        weapons.value.forEach { money.value += it.damage() }
     }
 
     fun buyWeapon(weapon: WeaponGame) {
         if (money.value >= weapon.upgradeCost()) {
-            money.value -= weapon.upgradeCost().toInt()
-            if (weapons.contains(weapon)) {
+            money.value -= weapon.upgradeCost()
+            if (weapons.value.contains(weapon)) {
                 weapon.upgrade()
             } else {
-            weapons.add(weapon)
+                weapons.value.add(weapon)
                 weapon.upgrade()
             }
 
